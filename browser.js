c9c5a23b9f3073f472c6f82d65046d1f8b51b9f8
Narazaka <info@narazaka.net>
2021-03-12 01:55:11 +0900

    impl

---
+++ browser.js
@@ -0,0 +1,40 @@
+/** @type {import("socket.io-client").io} */
+var io;
+
+const socket = io("ws://localhost:8000");
+
+function evtypelog(event) {
+    console.log(event.type)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const $input = document.querySelector("#input");
+    const $output = document.querySelector("#output");
+    /** @type {SpeechRecognition} */
+    const re = new (webkitSpeechRecognition || SpeechRecognition)();
+    re.onaudiostart = evtypelog
+    re.onaudioend = evtypelog
+    re.onerror = console.log
+    re.onstart = evtypelog
+    re.onnomatch = console.log
+    re.onsoundend = evtypelog
+    re.onsoundstart = evtypelog
+    re.onspeechstart = evtypelog
+    re.onspeechend = evtypelog
+    re.addEventListener("result", event => {
+        console.log(event)
+        
+        const result = event.results.item(0).item(0).transcript;
+        $input.textContent = result;
+        socket.send(result)
+    });
+    re.addEventListener("end", event => {
+        console.log(event)
+        re.start()
+    })
+    re.start();
+
+    socket.on("message", (data) => {
+        $output.textContent = data
+    })
+})
---
+++ server.js
@@ -0,0 +1,43 @@
+// @ts-check
+
+const Koa = require("koa");
+const IO = require('koa-socket-2');
+const WebSocket = require("ws");
+const fetch = require("node-fetch").default
+
+const apikey = require("./apiKey.json").apiKey;
+const port = 49513;
+
+const app = new Koa();
+const io = new IO();
+io.attach(app)
+
+const wss = new WebSocket(`ws://localhost:${port}`)
+
+app.use(require("koa-static")("."))
+
+const base = "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk";
+io.on("message", (ctx, query) => {
+    console.log(query)
+    const body = new URLSearchParams();
+    body.set("apikey", apikey)
+    body.set("query", query)
+    fetch(base, {
+        method: "POST",
+        body,
+    }).then(async res => {
+        const {status, message, results } = await res.json()
+        console.log(status, message, results)
+        if (!results) return;
+        const response = results[0].reply
+        /** @type {import("socket.io").Socket} */
+        const socket = ctx.socket
+        socket.send(response)
+        wss.send(`0:${response}`)
+    },
+    err => {
+        console.log(err)
+    })
+})
+
+app.listen(8000)
